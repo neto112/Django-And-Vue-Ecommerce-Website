375db590a36cb0920a3fe8fbf49192a1e6b37395
cristianorneto <cristianorneto@gmail.com>
2023-06-13 21:06:45 -0300

    feat: product pictures

--- apps/cart/cart.py
+++ apps/cart/cart.py
@@ -56,3 +56,9 @@ class Cart(object):
         print('save')
         self.session[settings.CART_SESSION_ID] = self.cart
         self.session.modified = True
+
+    def get_total_length(self):
+        return sum(int(item['quantity']) for item in self.cart.values())
+    
+    def get_total_cost(self):
+        return sum(float(item['total_price']) for item in self.cart.values())
--- saulgadgets/settings.py
+++ saulgadgets/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -43,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'apps.cart',
     'apps.core',
+    'apps.order',
     'apps.store'
 ]
 
@@ -126,6 +128,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
